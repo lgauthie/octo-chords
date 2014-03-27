76bcd6276168582a1b80dbab26bd4a763ac3ed22
eskat <skmoyls@gmail.com>
2014-03-26 17:45:01 -0700

    Pain
    THE ULTIMATE PAIN! mrw

--- test1.py
+++ test1.py
@@ -3,6 +3,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as p
+import math as m
 
 #import marsays as mar
 from marsyas import MarControlPtr
@@ -17,37 +18,54 @@ def main():
     with open(fname, 'r') as f:
         file_data = np.array([line.split() for line in f if len(line.split()) == 3])
     
-    time_data =  file_data[:,0:2]
+    time_data =  np.array([[float(item[0]), float(item[1])] for item in file_data[:,0:2]])
     chord_data = file_data[:,2:]
+    
+    # Get number of rows in time_data
+    (rows, _) = time_data.shape
+    
+    #print rows
+    #print cols
     
     #Testing
     #print time_data
     #print chord_data
-    
-    #Windows sizes given the beat information
-    window_sizes = []
-    
+    	
     netspec = ["Series/net",
     			["SoundFileSource/src", 
-    			 "Windowing/win",
     			 "Spectrum/spec",
     			 "PowerSpectrum/pspec",
     			 "Chroma/chroma",
     			 ]]
     net = create(netspec)
     
+    
     net.updControl("SoundFileSource/src/mrs_string/filename", fname2)
+    israte = net.getControl("mrs_real/israte").to_real()
     
     # Controls for Chroma
     #net.updControl("Chroma/mrs_natural/lowOctNum", ???)
     #net.updControl("Chroma/mrs_natural/highOctNum", ???)
     #net.updControl("Chroma/mrs_real/samplingFreq", ???)
-
+    
+    times = [MarControlPtr.from_natural(int((r[1]-r[0])*israte)) for r in time_data]
+    
+       #create chorma out matrix
+    for i, row in enumerate(time_data):
+        d_time = row[1]-row[0]
+    	nsamples = int(d_time*israte)
+    	#print nsamples
+    	net.updControl("mrs_natural/inSamples", MarControlPtr.from_natural(nsamples))
+    	net.tick()
+        #chroma = control2array(net, "mrs_realvec/processedData")
+        #print chroma
+    	
+"""
     while net.getControl("SoundFileSource/src/mrs_bool/hasData").to_bool():
         net.tick()
         chroma = control2array(net, "mrs_realvec/processedData")
-        print chroma
-    
+        #print chroma
+""" 
 
 if __name__ == "__main__":
     main()
