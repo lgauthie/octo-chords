b3f44e680e3553e96431f18189a2753fc6283893
Robert Janzen <robertjanzen@Robert-Janzens-MacBook-Pro.local>
2014-04-16 16:30:11 -0700

    Update chroma extraction to output csv

--- src/chroma_extract.py
+++ src/chroma_extract.py
@@ -18,9 +18,10 @@ def main():
     with open(fname, 'r') as f:
         file_data = np.array([line.split() for line in f if len(line.split()) == 3])
 
-    time_data =  np.array([[float(item[0]), float(item[1])] for item in file_data[:,0:2]])
+    time_data =  [(float(item[0]), float(item[1]), item[2]) for item in file_data[:,0:3]]
+    print time_data[1]
 
-    (rows, _) = time_data.shape
+    rows = len(time_data)
 
     netspec = ["Series/net",
         ["SoundFileSource/src",
@@ -30,7 +31,8 @@ def main():
          "Spectrum/spec",
          "PowerSpectrum/pspec",
          "Chroma/chroma",
-         #"Gain/dummy"
+         "Windowing/pad",
+         "WekaSink/wekout"
         ]]
     net = create(netspec)
 
@@ -40,6 +42,12 @@ def main():
     net.updControl("Windowing/window/mrs_natural/size", N)
     #net.updControl("Windowing/window/mrs_bool/zeroPhasing", MarControlPtr.from_bool(True))
 
+    # Pad to fix chroma output to arff, last chroma was missing as it was replaced by WekaSink with label
+    net.updControl("", )
+
+    net.updControl("WekaSink/wekout/mrs_string/filename", fname2.split('.')[0] + '.arff')
+    net.updControl("WekaSink/wekout/mrs_natural/nLabels", 1)
+
     israte = net.getControl("mrs_real/israte").to_real()
     inSamples = net.getControl("mrs_natural/inSamples");
 
@@ -47,22 +55,25 @@ def main():
     for row in time_data:
         nsamples = get_num_samples(row[1], row[0], israte)
         inSamples.setValue_natural(nsamples)
-        #net.updControl("Windowing/window/mrs_natural/size", 2048*6)
+        net.updControl("WekaSink/wekout/mrs_string/labelNames", row[2])
         net.tick()
+        #net.updControl("Windowing/window/mrs_natural/size", 2048*6)
         chroma = control2array(net, "mrs_realvec/processedData")
-        chromas = np.append(chromas, chroma)
+        print chroma.size
+        #chromas = np.append(chromas, chroma)
+
         #print net.getControl("Spectrum/spec/mrs_natural/inSamples").to_natural()
         #print chroma
     chromas = chromas.reshape(12,np.size(chromas)/12)
-    fig = plt.figure()
-    plt.clf()
-    ax = fig.add_subplot(111)
+    #fig = plt.figure()
+    #plt.clf()
+    #ax = fig.add_subplot(111)
     #ax.set_aspect(1)
     #ax.imshow(chromas, cmap='Blues', interpolation='nearest')
     #import ipdb; ipdb.set_trace()
     #ax.imshow(chromas[:,1000:1200], interpolation='nearest')
-    ax.pcolormesh(chromas)#, interpolation='nearest')
-    plt.show()
+    #ax.pcolormesh(chromas)#, interpolation='nearest')
+    #plt.show()
 
 def get_num_samples(x, y, srate):
     d_time = (x - y)
