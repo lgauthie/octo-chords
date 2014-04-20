b2a7ed2f0c2e38ad1d5a2f14626a9ad76da6acc5
Robert Janzen <robertjanzen@Robert-Janzens-MacBook-Pro.local>
2014-04-20 16:35:49 -0700

    Many changes

--- src/getchromas.sh
+++ src/getchromas.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 while read fn; do
-    echo $fn
     NUMBER=$(echo $fn | awk -F "/" '{print $6}')
-    echo $NUMBER
-    python chroma_extract.py /Volumes/Data/billboard/burgoyne2011chords/$NUMBER/mirex_triad.txt $fn
+    echo "python chroma_extract.py /Volumes/Data/billboard/burgoyne2011chords/$NUMBER/mirex_triad.txt $fn" >> commands.txt
 done < reg.mf
 
 while read fn; do
-    echo $fn
-    NUMBER=$(echo $fn | awk -F "/" $5)
-    python chroma_extract.py /Volumes/Data/billboard/burgoyne2011chords/$NUMBER/mirex_triad.txt $fn
+    NUMBER=$(echo $fn | awk -F "/" '{print $6}')
+    echo "python chroma_extract.py /Volumes/Data/billboard/burgoyne2011chords/$NUMBER/mirex_triad.txt $fn" >> commands.txt
 done < harm.mf
